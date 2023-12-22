f7d2dc68d3052c6e44288e68d0993bf5d3f47893
Oleg E. Vorobiov <isnullxbh@gmail.com>
2023-12-22 18:49:51 +0700

    Add a package tracking client for the Cainiao courier

---
+++ src/main/java/me/isnullxbh/judson/TrackingInfo.java
@@ -0,0 +1,23 @@
+package me.isnullxbh.judson;
+
+/**
+ * Contains information about the tracked package.
+ * @since 0.1.0
+ */
+public class TrackingInfo
+{
+    /** Current status setting time */
+    public String time;
+
+    /** Delivery status */
+    public String status;
+
+    /** Tracking number */
+    public String trackingNumber;
+
+    /** Origin country */
+    public String from;
+
+    /** Destination country */
+    public String to;
+}
